d0fe69bd46caa74b7987cb55673b6ad5b6bfd697
Denis <moiseev-denis@mail.ru>
2024-01-02 01:07:08 +0400

    Метод для выводы получившегося массива

--- Program.cs
+++ Program.cs
@@ -14,6 +14,18 @@
    return result;    
 }
 
+void PrintArray(string [] arr)
+{
+    Console.Write($"[");
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+
+    Console.WriteLine($"]");
+}
+
 string [] strArray1 = {"Hello", "2", "world", ":-)"};
 string [] strArray2 = {"123", "1567", "-2", "computer science"};
 string [] strArray3 = {"Russia", "Denmark", "Kazan", "Nim"};
@@ -22,3 +34,7 @@ Console.Clear();
 string [] showElemsWhichEqualAndLessThenThree1 = ShowElemsWhichEqualAndLessThenThree (strArray1);
 string [] showElemsWhichEqualAndLessThenThree2 = ShowElemsWhichEqualAndLessThenThree (strArray2);
 string [] showElemsWhichEqualAndLessThenThree3 = ShowElemsWhichEqualAndLessThenThree (strArray3);
+
+// Console.WriteLine($"Строковый массив по методу: [{string.Join(" ", showElemsWhichEqualAndLessThenThree1)}]");
+// Console.WriteLine($"Строковый массив по методу: [{string.Join(" ", showElemsWhichEqualAndLessThenThree2)}]");
+// Console.WriteLine($"Строковый массив по методу: [{string.Join(" ", showElemsWhichEqualAndLessThenThree3)}]");
